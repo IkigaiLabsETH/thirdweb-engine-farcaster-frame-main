2f335707de83359b6ebfc566956cb569053c77e2
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2024-03-02 16:38:55 +0100

    gm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta property="og:title" content={`Ikigai Labs XYZ Frame`} />
         <meta property="og:image" content={`/good.png`} />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content={`https://d391b93f5f62d9c15f67142e43841acc.ipfscdn.io/ipfs/bafybeibh2pki7uvifrhak36u45r4dy5w66chvzzayja4ndqp6i4wu2eb4m/0.png`} />
+        <meta property="fc:frame:image" content={`https://castart-livethelifetv.vercel.app/good.png`} />
         <meta property="fc:frame:post_url" content={`/good.png`} />
         <meta property="fc:frame:button:1" content="GM.LFG" />
       </Head>
