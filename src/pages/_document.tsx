46bfbf2d861522c1cb864fc95178440bca2386c6
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2024-03-02 16:32:18 +0100

    visual

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta property="og:title" content={`Ikigai Labs XYZ Frame`} />
-        <meta property="og:image" content={`/good.png`} />
+        <meta property="og:image" content={`/good.png>`} />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content={`/good.png`} />
+        <meta property="fc:frame:image" content={`1:1`} />
         <meta property="fc:frame:post_url" content={`/good.png`} />
         <meta property="fc:frame:button:1" content="GM.LFG" />
       </Head>
